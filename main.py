f36f8d303408547c8fc1e3f2a3400654970d4e27
Mak Ostrogonac <mak.ostrogonac@gmail.com>
2025-07-07 01:10:36 +0200

    Final working version

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-from stats import count_words, count_characters, count_characters_list
+from stats import count_characters, count_words
+import sys
+
+def sort_on(items):
+    return items["num"]
+
 def get_book_text(path):
     with open(path) as f:
         file_contents =  f.read()
         return file_contents
 
-def main():
-    book = get_book_text("./books/frankenstein.txt")
-    letters_count = count_characters(book)
-    print(letters_count)
-    
-main()
+
+if len(sys.argv) == 1:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+path = sys.argv[1]
+text = get_book_text(path)
+print("============ BOOKBOT ============")
+print(f"Analyzing book found at {path}...")
+print("----------- Word Count ----------")
+print(f"Found {count_words(text)} total words")
+print("--------- Character Count -------")
+result = count_characters(text)
+result.sort(key=sort_on, reverse=True)
+for letter in result:
+    print(letter["char"] + ": " + str(letter["num"]))
